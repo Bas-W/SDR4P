f53575af44c0856ee6afab086a0665e9a6f22a2d
BasW <bas.bw.welters@gmail.com>
2026-08-12 00:13:41 +0200

    Audio analyzer ringbuffer read template

--- core/src/utils/ring_buffer.h
+++ core/src/utils/ring_buffer.h
@@ -1,25 +1,90 @@
 #pragma once
+#include "Tracy.hpp"
+#include "flog.h"
 #include <cstdint>
+#include <cstring>
 #include <memory>
 #include <mutex>
 
 // Should be converted to template later
 
 namespace rbuf {
+    template <typename T>
     class SharedRingBuffer {
     public:
-        ~SharedRingBuffer();
+        ~SharedRingBuffer() {
+            freeBuf();
+        }
 
-        void init(uint32_t size);
-        void freeBuf();
-        void push(float* data, uint32_t count);
-        void read(float* dest, uint32_t offset, uint32_t count);
+        /// Initialize ring buffer
+        ///
+        /// Param size: amount of values to keep (not bytes)
+        void init(uint32_t size) {
+            ZoneScoped;
+            std::lock_guard<std::mutex> lck(m_mutex);
 
-        uint32_t getSize();
+            m_writeIdx = 0;
+            m_size = size;
+            m_buf = std::shared_ptr<T>(static_cast<T*>(std::malloc(size * sizeof(T))), free);
+        }
+
+        void freeBuf() {
+            ZoneScoped;
+            std::lock_guard<std::mutex> lck(m_mutex);
+            m_buf.reset();
+            m_size = 0;
+        }
+
+        /// Push data to ring buffer
+        void push(T* data, uint32_t count) {
+            ZoneScoped;
+            std::lock_guard<std::mutex> lck(m_mutex);
+
+            if (m_writeIdx + count <= m_size) {
+                std::memcpy(m_buf.get() + m_writeIdx, data, count * sizeof(T));
+                m_writeIdx += count;
+            }
+            else {
+                m_hasWrapped = true;
+                uint32_t remaining = count;
+                while (remaining > 0) {
+                    uint32_t nToAdd = std::min(remaining, m_size - m_writeIdx);
+                    std::memcpy(m_buf.get() + m_writeIdx, data + count - remaining, nToAdd * sizeof(T));
+                    m_writeIdx = (m_writeIdx + nToAdd) % m_size;
+                    remaining -= nToAdd;
+                }
+            }
+        }
+
+        /// Read from ringbuffer to regular buffer;
+        void read(T* dest, uint32_t offset, uint32_t count) {
+            ZoneScoped;
+            std::lock_guard<std::mutex> lck(m_mutex);
+
+            uint32_t idxToRead = (m_hasWrapped ? m_writeIdx : 0) + offset % m_size;
+            if (idxToRead + count <= m_size) {
+                std::memcpy(dest, m_buf.get() + idxToRead, count * sizeof(T));
+            }
+            else {
+                uint32_t remaining = count;
+                while (remaining > 0) {
+                    uint32_t nToRead = std::min(remaining, m_size - idxToRead);
+                    std::memcpy(dest + count - remaining, m_buf.get() + idxToRead, nToRead * sizeof(T));
+                    remaining -= nToRead;
+                    idxToRead = 0;
+                }
+            }
+        }
+
+        uint32_t getSize() {
+            ZoneScoped;
+            std::lock_guard<std::mutex> lck(m_mutex);
+            return m_size;
+        }
 
     private:
         std::mutex m_mutex;
-        std::shared_ptr<float> m_buf;
+        std::shared_ptr<T> m_buf;
         uint32_t m_writeIdx = 0;
         uint32_t m_size = 0;
         bool m_hasWrapped = false;
